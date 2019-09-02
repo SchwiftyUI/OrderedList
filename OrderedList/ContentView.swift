d93103e9675de543f5a1ebc2aa773ffcaf577e29
SchwiftyUI <schwiftyui@gmail.com>
2019-09-02 08:38:17 -0700

    Initial Commit

---
+++ OrderedList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OrderedList
+//
+//  Created by SchwiftyUI on 9/2/19.
+//  Copyright © 2019 SchwiftyUI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
